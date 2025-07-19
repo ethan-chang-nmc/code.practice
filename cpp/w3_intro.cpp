d83fa02629cd9588d560c2a257a792870bd093dc
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-18 19:29:40 -0500

    Update w3_intro.cpp
    fixed compiler issues

--- cpp/w3_intro.cpp
+++ cpp/w3_intro.cpp
@@ -11,5 +11,6 @@ Same basic data types as java
 int main()
 {
     int num1 = 2;
+    int x = 1, y = 2, z = 3;
     std::cout << "Hello World! " << num1 + 2 << "haha"; // alternative std:: for namespace
 }
