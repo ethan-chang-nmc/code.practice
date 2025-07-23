7e28b1950c4eb127141c9ccdc6e56a763443af04
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-23 11:46:51 -0500

    Update codio_obj_mutability.py
    codio mutability problem 2 solution

--- python/codio_obj_mutability.py
+++ python/codio_obj_mutability.py
@@ -18,5 +18,26 @@ class CelestialBody:
 planet = CelestialBody("Jupiter", 142984, 778360000, 79)
 print(planet.compared_to_earth())
 
+
 # Using the same CelestialBody class, write a static method closer_to_sun that compares two CelectialBody objects and returns 
 # the name of the object that is closes to the sun.
+class CelestialBody:
+    """Represents a celestial body"""
+    def __init__(self, name, diameter, distance, moons):
+        self.name = name
+        self.diameter = diameter
+        self.distance = distance
+        self.moons = moons
+        
+    @staticmethod
+    def closer_to_sun(cb1, cb2):
+        if cb1.distance < cb2.distance:
+            return cb1.name
+        else:
+            return cb2.name
+
+mercury = CelestialBody("Mercury", 4879.4, 57909000, 0)
+venus = CelestialBody("Venus", 12103.6, 108160000, 0)
+
+if __name__ == "__main__":
+    print (CelestialBody.closer_to_sun(mercury, venus))
