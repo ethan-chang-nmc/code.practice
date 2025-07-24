afcf7ae4ce62538a813629da608b1d428fad5487
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-24 12:21:20 -0500

    Update codio_encapsulation.py
    codio encapsulation problem 5 solution

--- python/codio_encapsulation.py
+++ python/codio_encapsulation.py
@@ -93,3 +93,32 @@ class Dancer:
 
 # Define the Cyclist class which has attributes name, nationality, and nickname. Use the Python convention to make these 
 # attributes private. Create the getters and setters using the property decorator.
+class Cyclist:
+    def __init__(self, name, nationality, nickname):
+       self._name = name
+       self._nationality = nationality
+       self._nickname = nickname
+
+    @property
+    def name(self):
+       return self._name
+    
+    @name.setter
+    def name(self, new_name):
+       self._name = new_name
+    
+    @property
+    def nationality(self):
+       return self._nationality
+    
+    @nationality.setter
+    def nationality(self, new_nat):
+       self._nationality = new_nat
+    
+    @property
+    def nickname(self):
+        return self._nickname
+    
+    @nickname.setter
+    def nickname(self, new_nickname):
+       self._nickname = new_nickname
