42c1990d5e1c74e48c5d9a4c36243ad2164c74f4
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-24 11:26:41 -0500

    Update codio_encapsulation.py
    codio encapsulation problem 1 solution

--- python/codio_encapsulation.py
+++ python/codio_encapsulation.py
@@ -9,3 +9,14 @@ __AttributeName can give appearance of private
 '''
 # Define the Country class which has attributes for name, capital, population, and 
 # continent. Please use the Python convention for making these attributes private.
+# You do not need to create any getters or setters; just follow the Python convention for private attributes.
+class Country:
+    def __init__(self, name, capital, population, continent):
+        self._name = name
+        self._capital = capital
+        self._population = population
+        self._continent = continent
+
+if __name__ == "__main__":
+    my_country = Country('France', 'Paris', 67081000, 'Europe')
+    print(my_country._name, my_country._capital, my_country._population, my_country._continent)
