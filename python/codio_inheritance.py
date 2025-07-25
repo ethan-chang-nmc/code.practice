91d92bd88d88820597fbd9ab3fe059e2249deaae
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-25 09:51:28 -0500

    Update codio_inheritance.py
    codio inheritance problem 1 solution

--- python/codio_inheritance.py
+++ python/codio_inheritance.py
@@ -8,3 +8,19 @@ Extention, Overriding, Multiple Inheritance
 # both of which are children of CelestialBody. Using the super() keyword to extend the __init__ method 
 # of the parent class, add the attribute host_planet to the Satellite class and add the attribute host_star 
 # to the Planet class.
+class CelestialBody:
+    def __init__(self, size, mass, composition, name):
+        self.size = size
+        self.mass = mass
+        self.composition = composition
+        self.name = name
+
+class Satellite(CelestialBody):
+    def __init__(self, size, mass, composition, name, host_planet):
+        super().__init__(size, mass, composition, name)
+        self.host_planet = host_planet
+
+class Planet(CelestialBody):
+    def __init__(self, size, mass, composition, name, host_star):
+        super().__init__(size, mass, composition, name)
+        self.host_star = host_star
