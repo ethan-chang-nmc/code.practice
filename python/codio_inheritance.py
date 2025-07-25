e5ae3a3927c9f7d607a3cbc61a6d484ee2649912
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-25 00:38:34 -0500

    Update codio_inheritance.py
    codio inheritance problem 1 description

--- python/codio_inheritance.py
+++ python/codio_inheritance.py
@@ -3,3 +3,7 @@ Notes:
 Method resolution order (.mro())
 Extention, Overriding, Multiple Inheritance
 '''
+# Use the CelestialBody class to the left as the parent class. Create the Satellite and Planet classes, 
+# both of which are children of CelestialBody. Using the super() keyword to extend the __init__ method 
+# of the parent class, add the attribute host_planet to the Satellite class and add the attribute host_star 
+# to the Planet class.
