2bc20c0491f86c2f1ffc1a20d43ee3e6f841ba8d
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-26 17:37:39 -0500

    Update codio_polymorphism.py
    codio polymorphism notes

---
+++ python/codio_polymorphism.py
@@ -0,0 +1,10 @@
+'''
+Notes:
+Polymorphism - single interface is applicable with different types (i.e. + operator)
+-> method overriding is a form
+Overloading - single method name that can take different sets of parameters
+-> also example of polymorphism
+-> when two methods have same name, python recognizes the last method (ignores rest)
+-> declare method with optional parameters set to None
+-> ex: print(int.__mul__(num1, num2)) for *, (int.__truediv__(num1, num2))
+'''
