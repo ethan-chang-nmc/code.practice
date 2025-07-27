3b0719f83ef6627e65bdf59daa43b48887895747
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-26 19:18:07 -0500

    Update codio_polymorphism.py
    codio polymorphism problem 2 description

--- python/codio_polymorphism.py
+++ python/codio_polymorphism.py
@@ -17,3 +17,22 @@ because, in this case, a single function works with objects of different types.
 '''
 # Use the Lottery class to the left as the parent class. Create the PowerBall class as a child class of 
 # Lottery. Override the shuffle method so that it returns a list of six random integers between 1 and 99.
+import random
+
+class Lottery:
+    def shuffle(self):
+        results = []
+        for i in range(5):
+            results.append(random.randint(1, 20))
+        return results
+
+class PowerBall(Lottery):
+    def shuffle(self):
+        results = []
+        for i in range(6):
+            results.append(random.randint(1,99))
+        return results
+
+if __name__ == "__main__":
+    output = PowerBall()
+    print(output.shuffle())
