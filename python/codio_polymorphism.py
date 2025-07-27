b4cb8eaefa02f8a71504be5aa4bb9a90e71a3747
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-26 22:26:06 -0500

    Update codio_polymorphism.py
    codio polymorphism problem 5 solution

--- python/codio_polymorphism.py
+++ python/codio_polymorphism.py
@@ -171,4 +171,52 @@ class Median:
 # The list_to_string method converts the 2D list back into a string.
 # When you override the swap_words methods, be sure to start with string_to_list and use list_to_string before writing to the file.
 # The text used for this activity are the first four sentences of Jane Austen’s Pride and Prejudice.
- 
+source_file = '/home/codio/workspace/code/polymorphism/text_1_exercise5.txt'
+answer_file = '/home/codio/workspace/code/polymorphism/answer_exercise5.txt'
+
+class Substitute:
+    def __init__(self, source_file, answer_file):
+        self.source_file = source_file
+        self.answer_file = answer_file
+        self.words = None
+    
+    def string_to_list(self):
+        '''Read text file, turn it into a
+        2D list of words for each line'''
+        words = []
+        with open(self.source_file, 'r') as file_object:
+            lines = file_object.read().split('\n')
+            for line in lines:
+                words.append(line.split())
+        self.words = words
+    
+    def list_to_string(self):
+        '''Convert 2D list back into a 
+        string with newline characters'''
+        lines = []
+        for line in self.words:
+            lines.append(' '.join(line))
+        string = '\n'.join(lines)
+        self.words = string
+  
+    def swap_words(self):
+        self.string_to_list()
+        for line in self.words:
+            for i in range(len(line)):
+                if (i + 1) % 5 == 0:
+                    word = line[i]
+                    line[i] = 'HELLO'
+        self.list_to_string()
+
+class Stars(Substitute):
+    def swap_words(self):
+        self.string_to_list()
+        for line in self.words:
+            for i in range(len(line)):
+                if (i + 1) % 3 == 0:
+                    word = line[i]
+                    line[i] = "*" * len(word)
+        self.list_to_string()
+
+        with open(self.answer_file, 'w') as f:
+            f.write(self.words)
