15f868db8239675666fc78b0f54d6b5e5406e50c
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-07-27 12:43:50 -0500

    Update codio_advtopic.py
    codio advtopic problem 1 description

--- python/codio_advtopic.py
+++ python/codio_advtopic.py
@@ -14,5 +14,11 @@ __repr__ method - information useful for developer
 -> use print(rep(obj)) otherwise will default to __str__
 -> usually one line of code (i.e: in __repr__ method, return f'Dog({self.name})
 -> __str__ default calls __repr__, which default return class name and object location
-
 '''
+# Write a script that imports the Phone and Laptop classes. These classes are found in the tech module. 
+# Your script must import the classes as follows:
+# import tech
+# Instantiate an instance of the Phone class and call it my_phone. It should have the name "Pixel 5", the 
+# color "sage", and 128 gigabytes of storage. Important, all numbers passed to the constructor should be integers.
+# Instantiate an instance of the Laptop class and call it my_laptop. It should have the name "MacBook Pro", 
+# a size of 15 inches, and 256 gigabytes of storage. Important, all numbers passed to the constructor should be integers.
